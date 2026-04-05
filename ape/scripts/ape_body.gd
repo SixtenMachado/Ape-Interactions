7d5a9579c2f40734047e4180ceaa277fae0c5889
SixtenMachado <sixten@sixten.com>
2026-04-06 00:07:51 +0200

    improved ape graphics and added body funny movement
    please make it less funny so that it can be funnier better

--- ape/scripts/ape_body.gd
+++ ape/scripts/ape_body.gd
@@ -6,6 +6,7 @@ class_name ApeBody
 @export var input: PlayerInput
 @export var camera: Camera3D
 @export var look: Node3D
+@export var model: Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting(&"physics/3d/default_gravity")
@@ -37,19 +38,22 @@ func _physics_process(delta: float) -> void:
 		# Add the gravity.
 		velocity.y -= gravity * delta
 	
-	# TODO: move this out to LookPivot, greater separation between camera and body
-	# Handle look left and right
-	rotate_object_local(Vector3(0, 1, 0), input.look_angle.x)
-
-	# Handle look up and down
-	look.rotate_object_local(Vector3(1, 0, 0), input.look_angle.y)
-
-	look.rotation.x = clamp(look.rotation.x, -1.57, 1.57)
-	look.rotation.z = 0
-	look.rotation.y = 0
-
+	## TODO: move this out to LookPivot, greater separation between camera and body
+	## Handle look left and right
+	#rotate_object_local(Vector3(0, 1, 0), input.look_angle.x)
+#
+	## Handle look up and down
+	#look.rotate_object_local(Vector3(1, 0, 0), input.look_angle.y)
+#
+	#look.rotation.x = clamp(look.rotation.x, -0.57, 1.57)
+	#look.rotation.z = 0
+	#look.rotation.y = 0
+	var adjusted_look_rotation := Vector3(0, look.rotation.y, 0) + Vector3(0, deg_to_rad(180), 0)
+	model.rotation = model.rotation.lerp(adjusted_look_rotation, delta * velocity.length())
+	
+	
 	# Apply movement
-	var input_dir = input.movement
+	var input_dir = input.movement.rotated(Vector3.UP, look.rotation.y)
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.z)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
--- ape/scripts/camera_pivot.gd
+++ ape/scripts/camera_pivot.gd
@@ -2,6 +2,18 @@ extends Node3D
 
 @export var input: PlayerInput
 
-func _rollback_tick(delta, _tick, _is_fresh):
-	pass
+func _physics_process(delta: float) -> void:
+	#DO BASIS SHIT
+	#Handle look left and right
+	#rotate(Vector3(0, 1, 0), input.look_angle.x)
+	rotation.y += (input.look_angle.x)
+	rotation.x += (input.look_angle.y)
+	#
+	## Handle look up and down
+	#rotate(Vector3(1, 0, 0), input.look_angle.y)
+
+	rotation.x = clamp(rotation.x, -0.57, 1.57)
+	rotation.z = 0
 #	TODO: put looking code here, then make player rotate to face camera when moving
+	
+	#position = get_parent().global_position
