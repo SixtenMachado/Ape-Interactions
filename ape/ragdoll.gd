89748fda2609e623fc274f45106cbb45bfb1589a
SixtenMachado <sixten@sixten.com>
2026-04-06 23:11:12 +0200

    ragedolls

--- ape/ragdoll.gd
+++ ape/ragdoll.gd
@@ -5,24 +5,27 @@ class_name Ragdoll
 @export var state : PlayerState
 @export var ape : ApeBody
 @export var skeleton : Skeleton3D
+@export var get_up_speed : float = 1.0
 
 var ragdolling : bool = false
 
-func _ready() -> void:
-	await get_tree().process_frame
-	physical_bones_start_simulation()
+
+#func _ready() -> void:
+	#await get_tree().process_frame
+	#physical_bones_start_simulation()
 
 func _physics_process(delta: float) -> void:
 	if input.ragdoll or state.current_state == state.State.RAGDOLL:
 		if not ragdolling:
-			for bone : PhysicalBone3D in get_children().filter(func(x): return x is PhysicalBone3D):
-				bone.transform = skeleton.get_bone_pose(bone.get_bone_id())
-		physical_bones_start_simulation()
-		#influence = lerpf(influence, 1, delta * 3)
-		ape.set_collision_layer_value(1, false)
-		ragdolling = true
+			physical_bones_stop_simulation()
+			physical_bones_start_simulation()
+			influence = 1
+			ape.set_collision_layer_value(1, false)
+			ragdolling = true
 	else:
-		physical_bones_stop_simulation()
-		#influence = lerpf(influence, 0, delta * 3)
+		if influence == 0:
+			physical_bones_stop_simulation()
+		else:
+			influence = clampf(influence - (delta * get_up_speed), 0, 1)
 		ape.set_collision_layer_value(1, true)
 		ragdolling = false
