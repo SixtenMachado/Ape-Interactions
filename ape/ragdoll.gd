93383bd1d928dbb2d46eec0adb81796b6ec11d41
SixtenMachado <sixten@sixten.com>
2026-04-06 21:14:46 +0200

    eugh

--- ape/ragdoll.gd
+++ ape/ragdoll.gd
@@ -4,39 +4,25 @@ class_name Ragdoll
 @export var input : PlayerInput
 @export var state : PlayerState
 @export var ape : ApeBody
+@export var skeleton : Skeleton3D
 
+var ragdolling : bool = false
+
+func _ready() -> void:
+	await get_tree().process_frame
+	physical_bones_start_simulation()
 
 func _physics_process(delta: float) -> void:
 	if input.ragdoll or state.current_state == state.State.RAGDOLL:
+		if not ragdolling:
+			for bone : PhysicalBone3D in get_children().filter(func(x): return x is PhysicalBone3D):
+				bone.transform = skeleton.get_bone_pose(bone.get_bone_id())
 		physical_bones_start_simulation()
+		#influence = lerpf(influence, 1, delta * 3)
 		ape.set_collision_layer_value(1, false)
-		if !is_multiplayer_authority():
-			network_update_bones(delta)
-		else:
-			ape.bone_data.clear()
-			for child in get_children():
-				if child is PhysicalBone3D:
-					var bone : PhysicalBone3D = child
-					ape.bone_data.append(bone.angular_velocity)
-					ape.bone_data.append(bone.linear_velocity)
-					ape.bone_transforms.append(bone.transform)
+		ragdolling = true
 	else:
 		physical_bones_stop_simulation()
+		#influence = lerpf(influence, 0, delta * 3)
 		ape.set_collision_layer_value(1, true)
-
-#TODO: figure out the smartest ape way to sync all the bones
-func network_update_bones(delta: float):
-	if is_multiplayer_authority(): return
-	var index : int = 0
-	if !ape.bone_transforms.is_empty():
-		for bone : PhysicalBone3D in get_children():
-			bone.angular_velocity = lerp(bone.angular_velocity, ape.bone_data.get(index), delta * 10)
-			index += 1
-			bone.linear_velocity = lerp(bone.linear_velocity, ape.bone_data.get(index), delta * 10)
-			index += 1
-	
-	var transform_index : int = 0
-	if !ape.bone_transforms.is_empty():
-		for bone : PhysicalBone3D in get_children():
-			bone.transform = lerp(bone.transform, ape.bone_transforms.get(transform_index), delta * 10)
-			transform_index += 1
+		ragdolling = false
